84d11467b5f5cc234efa9f2c25af511ef181218c
Xavier Martínez "Xavizard Knight <xaviergamerandom@gmail.com>
2023-01-15 17:22:25 +0100

    Huh... tablet mode goes BRRRRR

--- app/src/main/java/com/example/orgue/PantallaBotonsImatge.java
+++ app/src/main/java/com/example/orgue/PantallaBotonsImatge.java
@@ -3,6 +3,8 @@ package com.example.orgue;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Display;
+import android.view.WindowManager;
 
 public class PantallaBotonsImatge extends AppCompatActivity
 {
@@ -11,5 +13,9 @@ public class PantallaBotonsImatge extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_botons_imatge);
+
+        Display display = ((WindowManager)getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+        int width = display.getWidth();
+        int height = display.getHeight();
     }
 }
