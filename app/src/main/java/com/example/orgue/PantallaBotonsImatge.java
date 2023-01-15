ac967031a7b9ac2460cf0dad509e20f1100538b9
Xavier Martínez "Xavizard Knight <xaviergamerandom@gmail.com>
2023-01-15 18:41:23 +0100

    Trying to fight the UI

--- app/src/main/java/com/example/orgue/PantallaBotonsImatge.java
+++ app/src/main/java/com/example/orgue/PantallaBotonsImatge.java
@@ -2,6 +2,9 @@ package com.example.orgue;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.WindowManager;
@@ -17,5 +20,15 @@ public class PantallaBotonsImatge extends AppCompatActivity
         Display display = ((WindowManager)getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
         int width = display.getWidth();
         int height = display.getHeight();
+    }
+
+    public static void setComponentState(Context context, String packageName, String componentClassName, boolean enabled)
+    {
+        PackageManager pm  = context.getApplicationContext().getPackageManager();
+        ComponentName componentName = new ComponentName(packageName, componentClassName);
+        int state = enabled
+                ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+                : PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+        pm.setComponentEnabledSetting(componentName, state, PackageManager.DONT_KILL_APP);
     }
 }
