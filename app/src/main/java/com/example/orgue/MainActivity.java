fbb44bada3ba26fc52578c31780eaeb04edf9d76
Xavier Martínez "Xavizard Knight <xaviergamerandom@gmail.com>
2023-01-19 15:37:15 +0100

    MainActivity working and it goes to ImageButtonActivity like a boss

--- app/src/main/java/com/example/orgue/MainActivity.java
+++ app/src/main/java/com/example/orgue/MainActivity.java
@@ -8,24 +8,24 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
     private EditText nom;
     private ImageButton fletxa;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         nom = findViewById(R.id.etnom);
+        fletxa = findViewById(R.id.fletxa);
+        fletxa.setOnClickListener(v -> onClick());
     }
 
-    public void onClick(View v) {
-        //
-        // Uncomment when class ready, otherwise it errors on compile!
-        //
-
-        // startActivity(new Intent(MainActivity.this, SeguentActivity.class));
+    public void onClick()
+    {
+        startActivity(new Intent(MainActivity.this, PantallaBotonsImatge.class));
     }
 }
