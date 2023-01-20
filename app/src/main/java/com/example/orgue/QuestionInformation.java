eceb0e57219ac2ed6a287072e1da69bd2fccec76
Xavier Martínez "Xavizard Knight <xaviergamerandom@gmail.com>
2023-01-20 16:47:59 +0100

    Made the fields in QuestionInformation all public

--- app/src/main/java/com/example/orgue/QuestionInformation.java
+++ app/src/main/java/com/example/orgue/QuestionInformation.java
@@ -2,12 +2,12 @@ package com.example.orgue;
 
 public class QuestionInformation
 {
-    Class questionClass;
-    String questionTitle;
-    String[] images;
-    String[] texts;
-    String[] answers;
-    boolean aloneInItsType;
+    public Class questionClass;
+    public String questionTitle;
+    public String[] images;
+    public String[] texts;
+    public String[] answers;
+    public boolean aloneInItsType;
 
     public QuestionInformation()
     {
@@ -24,63 +24,63 @@ public class QuestionInformation
         this.aloneInItsType = aloneInItsType;
     }
 
-    public Class getQuestionClass()
-    {
-        return questionClass;
-    }
-
-    public void setQuestionClass(Class questionClass)
-    {
-        this.questionClass = questionClass;
-    }
-
-    public String getQuestionTitle()
-    {
-        return questionTitle;
-    }
-
-    public void setQuestionTitle(String questionTitle)
-    {
-        this.questionTitle = questionTitle;
-    }
-
-    public String[] getImages()
-    {
-        return images;
-    }
-
-    public void setImages(String[] images)
-    {
-        this.images = images;
-    }
-
-    public String[] getTexts()
-    {
-        return texts;
-    }
-
-    public void setTexts(String[] texts)
-    {
-        this.texts = texts;
-    }
-
-    public String[] getAnswers()
-    {
-        return answers;
-    }
-
-    public void setAnswers(String[] answers)
-    {
-        this.answers = answers;
-    }
-
-    public boolean isAloneInItsType()
-    {
-        return aloneInItsType;
-    }
-
-    public void setAloneInItsType(boolean aloneInItsType)
-    {
-        this.aloneInItsType = aloneInItsType;
-    }
+//    public Class getQuestionClass()
+//    {
+//        return questionClass;
+//    }
+//
+//    public void setQuestionClass(Class questionClass)
+//    {
+//        this.questionClass = questionClass;
+//    }
+//
+//    public String getQuestionTitle()
+//    {
+//        return questionTitle;
+//    }
+//
+//    public void setQuestionTitle(String questionTitle)
+//    {
+//        this.questionTitle = questionTitle;
+//    }
+//
+//    public String[] getImages()
+//    {
+//        return images;
+//    }
+//
+//    public void setImages(String[] images)
+//    {
+//        this.images = images;
+//    }
+//
+//    public String[] getTexts()
+//    {
+//        return texts;
+//    }
+//
+//    public void setTexts(String[] texts)
+//    {
+//        this.texts = texts;
+//    }
+//
+//    public String[] getAnswers()
+//    {
+//        return answers;
+//    }
+//
+//    public void setAnswers(String[] answers)
+//    {
+//        this.answers = answers;
+//    }
+//
+//    public boolean isAloneInItsType()
+//    {
+//        return aloneInItsType;
+//    }
+//
+//    public void setAloneInItsType(boolean aloneInItsType)
+//    {
+//        this.aloneInItsType = aloneInItsType;
+//    }
 }
