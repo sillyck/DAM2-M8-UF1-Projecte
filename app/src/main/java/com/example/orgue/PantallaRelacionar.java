263854bee49c6a2f56758c83b323231f89e749a7
Xavier Martínez "Xavizard Knight <xaviergamerandom@gmail.com>
2023-01-25 20:22:18 +0100

    Rectified PantallaRelacionar.java

--- app/src/main/java/com/example/orgue/PantallaRelacionar.java
+++ app/src/main/java/com/example/orgue/PantallaRelacionar.java
@@ -5,11 +5,15 @@ import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class PantallaRelacionar extends AppCompatActivity {
+public class PantallaRelacionar extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_relacionar);
+
+        
     }
 }
