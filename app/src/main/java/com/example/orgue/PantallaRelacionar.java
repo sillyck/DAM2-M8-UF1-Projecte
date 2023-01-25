9e71a3d084ccc576e6ef9625a699e55460c34ee9
Xavier Martínez "Xavizard Knight <xaviergamerandom@gmail.com>
2023-01-25 20:39:10 +0100

    fuap

--- app/src/main/java/com/example/orgue/PantallaRelacionar.java
+++ app/src/main/java/com/example/orgue/PantallaRelacionar.java
@@ -14,6 +14,6 @@ public class PantallaRelacionar extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relacionar);
 
-        
+
     }
 }
