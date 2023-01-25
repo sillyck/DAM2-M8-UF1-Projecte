d2533e2dc8c09406ca82d34dbee838968ae0ea26
Xavier Martínez "Xavizard Knight <xaviergamerandom@gmail.com>
2023-01-25 17:08:07 +0100

    Added ActivityCheckboxes.java

---
+++ app/src/main/java/com/example/orgue/ActivityCheckboxes.java
@@ -0,0 +1,15 @@
+package com.example.orgue;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityCheckboxes extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_boxes);
+    }
+}
