68cc062c41f7f6547fd55ec89cd5de8113270324
Xavier Martínez "Xavizard Knight <xaviergamerandom@gmail.com>
2023-01-27 16:17:11 +0100

    Javadoc in PantallaRelacionar

--- app/src/main/java/com/example/orgue/PantallaRelacionar.java
+++ app/src/main/java/com/example/orgue/PantallaRelacionar.java
@@ -2,20 +2,37 @@ package com.example.orgue;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * Classe de l'activitat de relacionar amb dropdowns/spinners.
+ */
 public class PantallaRelacionar extends AppCompatActivity
 {
+    /**
+     * Estat de la pregunta. Si esta en 0, accepta respostes; 1: contestada.
+     */
     public int state = 0;
+
+    /**
+     * Array a on es guardará si cada dropdown s'ha respós bé.
+     * -1: encara no contestat; 0: resposta incorrecta; 1: resposta correcta.
+     */
     public int[] didItGetItRight = new int[]{-1,-1,-1,-1};
+
+    /**
+     * Com el didItGetItRight, pero en general. De tota la pregunta.
+     * -1: encara no contestat; 0: resposta incorrecta; 1: resposta correcta.
+     */
     public int didItGetItRightInGeneral = -1;
 
-    public int[] correctAnswersInt = new int[]{3,0,2,1};
+    /**
+     * Array amb les respostes correctes en l'ordre correcte.
+     */
     public String[] correctAnswers = new String[]
     {
         "Té milers de tubs i necessita un espai gran per a posar-lo",
@@ -24,6 +41,9 @@ public class PantallaRelacionar extends AppCompatActivity
         "Té només un teclat però ja té dimensions considerables"
     };
 
+    /**
+     * Array amb les respostes possibles.
+     */
     public String[] dropdownOptions = new String[]
     {
         "Té només un teclat però ja té dimensions considerables",
@@ -32,9 +52,21 @@ public class PantallaRelacionar extends AppCompatActivity
         "Té milers de tubs i necessita un espai gran per a posar-lo"
     };
 
+    /**
+     * Els dropdowns de la pantalla.
+     */
     public Spinner dropdown1, dropdown2, dropdown3, dropdown4;
+
+    /**
+     * L'ImageButton principal de la pantalla.
+     */
     public ImageButton imageButton;
 
+    /**
+     * @param savedInstanceState If the activity is being re-initialized after
+     *                           previously being shut down then this Bundle contains the data it most
+     *                           recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     */
     @SuppressWarnings("Convert2Diamond")
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -59,10 +91,11 @@ public class PantallaRelacionar extends AppCompatActivity
         dropdown2.setAdapter(adapterView1);
         dropdown3.setAdapter(adapterView1);
         dropdown4.setAdapter(adapterView1);
-
-
     }
 
+    /**
+     *  Funció que s'executa en fer click al ImageButton.
+     */
     private void onClick()
     {
         switch(state)
@@ -77,67 +110,49 @@ public class PantallaRelacionar extends AppCompatActivity
                 break;
             case 1:
                 int corrects = 0;
-
                 for(int i=0; i<didItGetItRight.length; i++) if(didItGetItRight[i]==1) corrects++;
                 LogicSingleton.PushMoreScores(corrects,didItGetItRight.length);
                 startActivity(LogicSingleton.NextQuestion(PantallaRelacionar.this));
-                break;
+            break;
         }
     }
 
+    /**
+     * Metode que comprova les respostes dels dropdowns i despres les pinta segons correspongui.
+     * Increïble 2x1!!
+     */
+    @SuppressWarnings({"ConstantConditions", "UnusedAssignment"})
     private void checkAnswers()
     {
-//        if(dropdown1.getPrompt().toString().equals(correctAnswers[0]))
         if(dropdown1.getSelectedItem().toString().equals(correctAnswers[0]))
         {
             didItGetItRight[0] = 1;
         }
         else didItGetItRight[0] = 0;
-//        if(dropdown2.getPrompt().toString().equals(correctAnswers[1]))
         if(dropdown2.getSelectedItem().toString().equals(correctAnswers[1]))
         {
             didItGetItRight[1] = 1;
         }
         else didItGetItRight[1] = 0;
-//        if(dropdown3.getPrompt().toString().equals(correctAnswers[2]))
         if(dropdown3.getSelectedItem().toString().equals(correctAnswers[2]))
         {
             didItGetItRight[2] = 1;
         }
         else didItGetItRight[2] = 0;
-//        if(dropdown4.getPrompt().toString().equals(correctAnswers[3]))
         if(dropdown4.getSelectedItem().toString().equals(correctAnswers[3]))
         {
             didItGetItRight[3] = 1;
         }
         else didItGetItRight[3] = 0;
 
-        for(int i=0; i<didItGetItRight.length; i++)
+        if(dropdown1.getSelectedItem().toString().equals(correctAnswers[0])
+                && dropdown2.getSelectedItem().toString().equals(correctAnswers[1])
+                && dropdown3.getSelectedItem().toString().equals(correctAnswers[2])
+                && dropdown4.getSelectedItem().toString().equals(correctAnswers[3]))
         {
-            System.out.println("didItGetItRight["+i+"] = " + didItGetItRight[i]);
-        }
-
-//        if(dropdown1.getPrompt().toString().equals(correctAnswers[0])
-//            && dropdown2.getPrompt().toString().equals(correctAnswers[1])
-//            && dropdown3.getPrompt().toString().equals(correctAnswers[2])
-//            && dropdown4.getPrompt().toString().equals(correctAnswers[3]))
-        System.out.println("1");
-        if(dropdown1.getSelectedItem().toString().equals(correctAnswers[0]))
-            if(dropdown2.getSelectedItem().toString().equals(correctAnswers[1]))
-                if(dropdown3.getSelectedItem().toString().equals(correctAnswers[2]))
-                    if(dropdown4.getSelectedItem().toString().equals(correctAnswers[3]))
-        {
-            System.out.println("2");
             didItGetItRightInGeneral = 1;
         }
-//        else
-//        {
-            System.out.println("3");
-            didItGetItRightInGeneral = 0;
-//        }
-        System.out.println("4");
-
-        System.out.println("didItGetItRightInGeneral = " + didItGetItRightInGeneral);
+        didItGetItRightInGeneral = 0;
 
         if(didItGetItRightInGeneral==1)
         {
