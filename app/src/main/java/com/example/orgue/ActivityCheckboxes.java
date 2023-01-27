b798d5cfb65ea2dac0e850d37ffaf7f749f142e2
Xavier Martínez "Xavizard Knight <xaviergamerandom@gmail.com>
2023-01-27 16:16:38 +0100

    Javadoc in ActivtyCheckboxes

--- app/src/main/java/com/example/orgue/ActivityCheckboxes.java
+++ app/src/main/java/com/example/orgue/ActivityCheckboxes.java
@@ -7,21 +7,77 @@ import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * Activitat de les checkboxes.
+ */
 public class ActivityCheckboxes extends AppCompatActivity
 {
+    /**
+     *  Estat de la pregunta.
+     *  0: Pagina de pregunta; 1: pagina de respondre; 2: respós.
+     */
     public int state = 0;
+
+    /**
+     * Array a on es guardará si cada dropdown s'ha respós bé.
+     * -1: encara no contestat; 0: resposta incorrecta; 1: resposta correcta.
+     */
     public int[] didItGetItRight = new int[]{-1,-1,-1,-1};
+
+    /**
+     * Com el didItGetItRight, pero en general. De tota la pregunta.
+     * -1: encara no contestat; 0: resposta incorrecta; 1: resposta correcta.
+     */
     public int didItGetItRightInGeneral = -1;
 
-    public boolean[] correctAnswers = new boolean[]{false,false,true,false,true,true,true,false,true};
-    public boolean[] currentAnswers = new boolean[]{false,false,false,false,false,false,false,false,false};
-    public boolean[] paintableAnswers = new boolean[]{false,false,false,false,false,false,false,false,false};
+    /**
+     * Array de les respostes correctes de les checkboxes.
+     */
+    public boolean[] correctAnswers = new boolean[]
+    {
+        false, false, true,
+        false, true,  true,
+        true,  false, true
+    };
 
+    /**
+     * Array de les respostes que l'usuari ha posat.
+     */
+    public boolean[] currentAnswers = new boolean[]
+    {
+        false, false, false,
+        false, false, false,
+        false, false, false
+    };
+
+    /**
+     *  Array de les respostes segons si coincideixen entre les del jugador i les correctes.
+     *  Quan el jugador respongui a tot, els true d'aqui serán pintat en verd i els false en vermell.
+     */
+    public boolean[] paintableAnswers = new boolean[]
+    {
+        false,false,false,
+        false,false,false,
+        false,false,false
+    };
+
+    /**
+     * Totes les checkboxes.
+     */
     public CheckBox checkBox1,checkBox2,checkBox3,
                     checkBox4,checkBox5,checkBox6,
                     checkBox7,checkBox8,checkBox9;
+
+    /**
+     * ImageButton principal.
+     */
     public ImageButton imageButton;
 
+    /**
+     * @param savedInstanceState If the activity is being re-initialized after
+     *                           previously being shut down then this Bundle contains the data it most
+     *                           recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -50,22 +106,20 @@ public class ActivityCheckboxes extends AppCompatActivity
         checkBox9.setOnClickListener(v -> alterCheckbox(8));
     }
 
-    public void alterCheckbox(int checkbox)
+    /**
+     * Funció que gira l'estat d'una checkbox just despres que el jugador l'hi faci click.
+     * @param checkbox La checkbox a girar.
+     */
+    private void alterCheckbox(int checkbox)
     {
-//        switch(checkbox)
-//        {
-//            case 1:
-                if(currentAnswers[checkbox]==true)
-                {
-                    currentAnswers[checkbox]=false;
-                }
-                else
-                {
-                    currentAnswers[checkbox]=true;
-                }
-//        }
+        if(currentAnswers[checkbox]) currentAnswers[checkbox]=false;
+        else currentAnswers[checkbox]=true;
     }
 
+    /**
+     * Funció que s'executa en fer click al ImageButton.
+     * Comprova l'estat de les respostes o canvia d'activitat.
+     */
     private void onClick()
     {
         if(state==0)
@@ -92,6 +146,9 @@ public class ActivityCheckboxes extends AppCompatActivity
         }
     }
 
+    /**
+     * Omple el array de paintableAnswers que conté informació de quines checkboxes s'han de pintar verd o vermell.
+     */
     private void setPaintableAnswers()
     {
         for(int i=0; i<paintableAnswers.length; i++)
@@ -101,25 +158,82 @@ public class ActivityCheckboxes extends AppCompatActivity
         }
     }
 
+    /**
+     * Pinta les checkboxes segons si estan correctes o incorrectes.
+     */
     private void paintCheckboxes()
     {
-        if(paintableAnswers[0]==true) checkBox1.setBackgroundColor(Color.rgb(162,240,163));
-        else checkBox1.setBackgroundColor(Color.rgb(225,123,123));
-        if(paintableAnswers[1]==true) checkBox2.setBackgroundColor(Color.rgb(162,240,163));
-        else checkBox2.setBackgroundColor(Color.rgb(225,123,123));
-        if(paintableAnswers[2]==true) checkBox3.setBackgroundColor(Color.rgb(162,240,163));
-        else checkBox3.setBackgroundColor(Color.rgb(225,123,123));
-        if(paintableAnswers[3]==true) checkBox4.setBackgroundColor(Color.rgb(162,240,163));
-        else checkBox4.setBackgroundColor(Color.rgb(225,123,123));
-        if(paintableAnswers[4]==true) checkBox5.setBackgroundColor(Color.rgb(162,240,163));
-        else checkBox5.setBackgroundColor(Color.rgb(225,123,123));
-        if(paintableAnswers[5]==true) checkBox6.setBackgroundColor(Color.rgb(162,240,163));
-        else checkBox6.setBackgroundColor(Color.rgb(225,123,123));
-        if(paintableAnswers[6]==true) checkBox7.setBackgroundColor(Color.rgb(162,240,163));
-        else checkBox7.setBackgroundColor(Color.rgb(225,123,123));
-        if(paintableAnswers[7]==true) checkBox8.setBackgroundColor(Color.rgb(162,240,163));
-        else checkBox8.setBackgroundColor(Color.rgb(225,123,123));
-        if(paintableAnswers[8]==true) checkBox9.setBackgroundColor(Color.rgb(162,240,163));
-        else checkBox9.setBackgroundColor(Color.rgb(225,123,123));
+        if(paintableAnswers[0])
+        {
+            checkBox1.setBackgroundColor(Color.rgb(162,240,163));
+        }
+        else
+        {
+            checkBox1.setBackgroundColor(Color.rgb(225,123,123));
+        }
+        if(paintableAnswers[1])
+        {
+            checkBox2.setBackgroundColor(Color.rgb(162,240,163));
+        }
+        else
+        {
+            checkBox2.setBackgroundColor(Color.rgb(225,123,123));
+        }
+        if(paintableAnswers[2])
+        {
+            checkBox3.setBackgroundColor(Color.rgb(162,240,163));
+        }
+        else
+        {
+            checkBox3.setBackgroundColor(Color.rgb(225,123,123));
+        }
+        if(paintableAnswers[3])
+        {
+            checkBox4.setBackgroundColor(Color.rgb(162,240,163));
+        }
+        else
+        {
+            checkBox4.setBackgroundColor(Color.rgb(225,123,123));
+        }
+        if(paintableAnswers[4])
+        {
+            checkBox5.setBackgroundColor(Color.rgb(162,240,163));
+        }
+        else
+        {
+            checkBox5.setBackgroundColor(Color.rgb(225,123,123));
+        }
+        if(paintableAnswers[5])
+        {
+            checkBox6.setBackgroundColor(Color.rgb(162,240,163));
+        }
+        else
+        {
+            checkBox6.setBackgroundColor(Color.rgb(225,123,123));
+        }
+        if(paintableAnswers[6])
+        {
+            checkBox7.setBackgroundColor(Color.rgb(162,240,163));
+        }
+        else
+        {
+            checkBox7.setBackgroundColor(Color.rgb(225,123,123));
+        }
+        if(paintableAnswers[7])
+        {
+            checkBox8.setBackgroundColor(Color.rgb(162,240,163));
+        }
+        else
+        {
+            checkBox8.setBackgroundColor(Color.rgb(225,123,123));
+        }
+        if(paintableAnswers[8])
+        {
+            checkBox9.setBackgroundColor(Color.rgb(162,240,163));
+        }
+        else
+        {
+            checkBox9.setBackgroundColor(Color.rgb(225,123,123));
+        }
     }
 }
