d65089090d8ebaf47fbe0ea5f4e786cc0b5c5dca
Xavier Martínez "Xavizard Knight <xaviergamerandom@gmail.com>
2023-01-27 16:16:47 +0100

    Javadoc in MainActivity

--- app/src/main/java/com/example/orgue/MainActivity.java
+++ app/src/main/java/com/example/orgue/MainActivity.java
@@ -2,16 +2,31 @@ package com.example.orgue;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+/**
+ *  Classe principal de l'activitat principal; la primera pantalla de totes;
+ *  a on el jugador posa el seu nom.
+ */
 public class MainActivity extends AppCompatActivity
 {
+    /**
+     * El camp de text on el jugador posa el seu nom.
+     */
     private EditText nom;
+
+    /**
+     * ImageButton que al pretar, comença el joc.
+     */
     private ImageButton fletxa;
 
+    /**
+     * @param savedInstanceState If the activity is being re-initialized after
+     *                           previously being shut down then this Bundle contains the data it most
+     *                           recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -21,10 +36,12 @@ public class MainActivity extends AppCompatActivity
         nom = findViewById(R.id.etnom);
         fletxa = findViewById(R.id.fletxa);
         fletxa.setOnClickListener(v -> onClick());
-
-//        startActivity(new Intent(MainActivity.this, elnom de la pantalla));
     }
 
+    /**
+     * Funció que s'executa en fer click al ImageButton principal, inicialitza el singleton iç
+     * comença a per la primera pregunta.
+     */
     public void onClick()
     {
         if(!nom.getText().toString().isEmpty())
