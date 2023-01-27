6e1280772cf3d53ae50de1a5a6872572cf76404b
Xavier Martínez "Xavizard Knight <xaviergamerandom@gmail.com>
2023-01-27 10:47:34 +0100

    Javadoc and better code in LogicSingleton.java

--- app/src/main/java/com/example/orgue/LogicSingleton.java
+++ app/src/main/java/com/example/orgue/LogicSingleton.java
@@ -8,20 +8,63 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
+/**
+ * Classe estatica que guardará el nom del jugador, les puntuacions, les preguntes i tindrá
+ * els metodes per canviar de preguntes.
+ */
 public class LogicSingleton
 {
+
+    /**
+     * El nom del jugador.
+     */
     private static String playerName;
+
+    /**
+     * El numero que representa la pregunta actual
+     */
     private static int currentQuestion;
+
+    /**
+     * Els encerts del jugador.
+     */
     public static int currentScore;
+
+    /**
+     * El numero de respostes que el jugador ha respós tan com bé o malament.
+     */
     public static int totalScore;
+
+    /**
+     * System.currentTimeMillis() que s'executa en inicialitzar el singleton.
+     */
     private static long timeStart;
+
+    /**
+     * System.currentTimeMills() que s'executa en el metode de "pausar el timer".
+     */
     private static long timeFinish;
 
+    /**
+     * Duració dels temps que ha tardat en el jugar en arribar fins aqui, en segons
+     */
     public static int durationSeconds;
+
+    /**
+     * Duració dels temps que ha tardat en el jugar en arribar fins aqui, en minuts
+     */
     public static int durationMinutes;
 
+    /**
+     * La base de dades de les preguntes; aqui dins s'enmagatzema la informació que s'ugtilitzar per
+     * canviar de pantalla i per complir camps d'algunes preguntes.
+     */
     private static Map<Integer,QuestionInformation> questionDatabase;
 
+    /**
+     * "Inicialitza" el singleton. Funciona com un constructor, per tal de posar totes les variables
+     * a zero i/o a punt per el seu primer ús.
+     */
     public static void Initialize()
     {
         timeStart = System.currentTimeMillis();
@@ -31,6 +74,10 @@ public class LogicSingleton
         PushInformationToDatabase();
     }
 
+    /**
+     * Pausa el timer. En veritat no hi ha cap timer aqui i no pausa res. pero aquest metode executa
+     * mates per tal de calcular quans segons i minuts ha tardat el jugador en arribar fins aqui.
+     */
     public static void StopTimer()
     {
         timeFinish = System.currentTimeMillis();
@@ -38,26 +85,49 @@ public class LogicSingleton
         durationMinutes = (int)((float)durationSeconds/60);
     }
 
+    /**
+     * Setter de playerName
+     * @param newPlayerName El nou nom del jugador
+     */
     public static void SetNewPlayerName(String newPlayerName)
     {
         playerName = newPlayerName;
     }
 
+    /**
+     * El getter de playerName
+     * @return El nom del jugador
+     */
     public static String getPlayerName()
     {
         return playerName;
     }
 
+    /**
+     * Getter de currentQuestion.
+     * @return El numero corresponent a la pregunta acutal.
+     */
     public static int GetCurrentQuestion()
     {
         return currentQuestion;
     }
 
+    /**
+     * Retorna la informació de la pregunta actual.
+     * @return La informació de la pregunta actual segons la questionDatabase.
+     */
     public static QuestionInformation getCurrentQuestionInformation()
     {
         return questionDatabase.get(currentQuestion);
     }
 
+    /**
+     * Metode que retorna l'Intent de la seguent pregunta per tal que la classe de l'activitat
+     * hi pugui anar.
+     * @param currentContext El Context de l'activitat actual, s'utilitzaraá per inicialitzar
+     *                       l'Intent que es retornará.
+     * @return L'Intent que la classe utilitzará per canviar d'activity.
+     */
     public static Intent NextQuestion(Context currentContext)
     {
         currentQuestion++;
@@ -65,33 +135,47 @@ public class LogicSingleton
         throw new NullPointerException();
     }
 
+    /**
+     * Suma mes puntuacions a les actuals.
+     * @param current Les respostes correctes de la pantalla actual.
+     * @param total Respostes totals.
+     */
     public static void PushMoreScores(int current, int total)
     {
         currentScore += current;
         totalScore += total;
     }
 
+    /**
+     * Construeix un Intent en base a quina pregunta s'hagi de cargar i d'a on estigui ara mateix.
+     * @param currentContext El Context de la pantalla actual.
+     * @param num La pregunta que s'ha de cargar.
+     * @return L'Intent ja ben format.
+     */
     private static Intent WhatActivityShouldBeLoaded(Context currentContext, int num)
     {
         if(questionDatabase.containsKey(num)) return new Intent(currentContext, Objects.requireNonNull(questionDatabase.get(num)).questionClass);
         else return null;
     }
 
+    /**
+     * Metode que posa tota la informació a la base de dades de preguntes.
+     */
     private static void PushInformationToDatabase()
     {
-//        questionDatabase.put
-//        (
-//            0,
-//            new QuestionInformation
-//            (
-//                MainActivity.class,
-//                "",
-//                new String[]{""},
-//                new String[]{""},
-//                new String[]{""},
-//                true
-//            )
-//        );
+        questionDatabase.put
+        (
+            0,
+            new QuestionInformation
+            (
+                MainActivity.class,
+                "",
+                new String[]{""},
+                new String[]{""},
+                new String[]{""},
+                true
+            )
+        );
         questionDatabase.put
         (
             1,
@@ -196,29 +280,5 @@ public class LogicSingleton
                 true
             )
         );
-    }
-
-    public int calculateCurrentQuestion(String input)
-    {
-        switch(input)
-        {
-            case "1.2": return 1;
-            case "1.4": return 1;
-            case "2.2": return 2;
-            case "3.2": return 3;
-            case "3.3": return Integer.parseInt(null);
-            case "3.6": return 4;
-            case "4.1": return Integer.parseInt(null);
-            case "4.4": return 5;
-            case "5.2": return 6;
-            case "5.4": return 7;
-            case "6.2": return 8;
-            case "6.5": return 9;
-            case "7.2": return Integer.parseInt(null);
-            case "7.4": return 10;
-            case "8": return 11;
-            case "0": return 0;
-            default: return 0;
-        }
     }
 }
