38bf957be2bb3c883c0b3b455b85d1c941886896
Xavier Martínez "Xavizard Knight <xaviergamerandom@gmail.com>
2023-03-22 19:40:07 +0100

    MainMenu launcher activity now functional

--- app/src/main/java/com/example/orgue/MainMenu.java
+++ app/src/main/java/com/example/orgue/MainMenu.java
@@ -2,13 +2,21 @@ package com.example.orgue;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainMenu extends AppCompatActivity {
-
+public class MainMenu extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        findViewById(R.id.joc).setOnClickListener(v -> startActivity(new Intent(MainMenu.this, MainActivity.class)));
+        findViewById(R.id.musica).setOnClickListener(v -> startActivity(new Intent(MainMenu.this, Final.class)));
+        findViewById(R.id.preferencies).setOnClickListener(v -> startActivity(new Intent(MainMenu.this, Final.class)));
+        findViewById(R.id.tancarButton).setOnClickListener(v -> finishAffinity());
+
     }
 }
