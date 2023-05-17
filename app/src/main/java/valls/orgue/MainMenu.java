6d4ac3983cffc02eb694cfc04b9d62f673e2bd03
Xavier Martínez "Xavizard Knight <xaviergamerandom@gmail.com>
2023-05-17 13:39:10 +0200

    Refactoring towards caos

--- app/src/main/java/valls/orgue/MainMenu.java
+++ app/src/main/java/valls/orgue/MainMenu.java
@@ -1,93 +1,113 @@
 package valls.orgue;
 
+import static androidx.core.app.ActivityCompat.finishAffinity;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MainMenu extends AppCompatActivity
+public class MainMenu extends Fragment
 {
+    private static final String TAG = "MainMenu";
+    private View v;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance)
     {
+        return inflater.inflate(R.layout.activity_main_menu, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState)
+    {
+        super.onActivityCreated(savedInstanceState);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+//        setContentView(R.layout.activity_main_menu);
 
-        AudioHolder.Start(MainMenu.this);
+        v = getView();
 
-        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        AudioHolder.Start(v.getContext());
 
-        AudioHolder.canPlayBGM = pref.getBoolean("musica",true);
-        AudioHolder.canPlaySFX = pref.getBoolean("sfx_btn",true);
-        AudioHolder.canPlayOkKo = pref.getBoolean("sfx_correct",true);
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(v.getContext());
 
-        findViewById(R.id.tv_play).setOnClickListener(v ->
-        {
-//            AudioHolder.PlaySfx(Sound.Standard);
-//            AudioHolder.PlayBgm();
-            AudioHolder.mediaPlayerMusic.stop();
-            startActivity(new Intent(MainMenu.this, MainActivity.class));
+        AudioHolder.canPlayBGM = pref.getBoolean("musica", true);
+        AudioHolder.canPlaySFX = pref.getBoolean("sfx_btn", true);
+        AudioHolder.canPlayOkKo = pref.getBoolean("sfx_correct", true);
+
+        v.findViewById(R.id.tv_play).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//              AudioHolder.PlaySfx(Sound.Standard);
+//              AudioHolder.PlayBgm();
+                AudioHolder.mediaPlayerMusic.stop();
+                startActivity(new Intent(v.getContext(), MainActivity.class));
+            }
         });
-        findViewById(R.id.tv_play2).setOnClickListener(v ->
+        v.findViewById(R.id.tv_play2).setOnClickListener(v ->
         {
-            if(AudioHolder.canPlaySFX) AudioHolder.PlaySfx(Sound.Standard);
-            if(AudioHolder.mediaPlayer.isPlaying()) AudioHolder.StopBgm();
+            if (AudioHolder.canPlaySFX) AudioHolder.PlaySfx(Sound.Standard);
+            if (AudioHolder.mediaPlayer.isPlaying()) AudioHolder.StopBgm();
 //            AudioHolder.mediaPlayer.pause();
             AudioHolder.mediaPlayerMusic.stop();
-            startActivity(new Intent(MainMenu.this, Music.class));
+            startActivity(new Intent(v.getContext(), Music.class));
         });
-        findViewById(R.id.imgBtn_settings).setOnClickListener(v ->
+        v.findViewById(R.id.imgBtn_settings).setOnClickListener(v ->
         {
-            if(AudioHolder.canPlaySFX) AudioHolder.PlaySfx(Sound.Standard);
-            if(AudioHolder.mediaPlayer.isPlaying()) AudioHolder.mediaPlayer.pause();
+            if (AudioHolder.canPlaySFX) AudioHolder.PlaySfx(Sound.Standard);
+            if (AudioHolder.mediaPlayer.isPlaying()) AudioHolder.mediaPlayer.pause();
             AudioHolder.mediaPlayerMusic.stop();
-            startActivity(new Intent(MainMenu.this, Preferencies.class));
+            startActivity(new Intent(v.getContext(), Preferencies.class));
         });
-        findViewById(R.id.imgBtn_exit).setOnClickListener(v ->
+        v.findViewById(R.id.imgBtn_exit).setOnClickListener(v ->
         {
             AudioHolder.mediaPlayerMusic.release();
             AudioHolder.soundPool.release();
             AudioHolder.mediaPlayer.release();
-            finishAffinity();
+            getActivity().finishAffinity();
         });
 
-        findViewById(R.id.imgBtn_play).setOnClickListener(v ->
+        v.findViewById(R.id.imgBtn_play).setOnClickListener(v ->
         {
 //            AudioHolder.PlaySfx(Sound.Standard);
 //            AudioHolder.PlayBgm();
             AudioHolder.mediaPlayerMusic.stop();
-            startActivity(new Intent(MainMenu.this, MainActivity.class));
+            startActivity(new Intent(v.getContext(), MainActivity.class));
         });
-        findViewById(R.id.imgBtn_music).setOnClickListener(v ->
+        v.findViewById(R.id.imgBtn_music).setOnClickListener(v ->
         {
-            if(AudioHolder.canPlaySFX) AudioHolder.PlaySfx(Sound.Standard);
-            if(AudioHolder.mediaPlayer.isPlaying()) AudioHolder.StopBgm();
+            if (AudioHolder.canPlaySFX) AudioHolder.PlaySfx(Sound.Standard);
+            if (AudioHolder.mediaPlayer.isPlaying()) AudioHolder.StopBgm();
 //            AudioHolder.mediaPlayer.pause();
             AudioHolder.mediaPlayerMusic.stop();
-            startActivity(new Intent(MainMenu.this, Music.class));
+            startActivity(new Intent(v.getContext(), Music.class));
         });
-        findViewById(R.id.imgBtn_info).setOnClickListener(v ->
+        v.findViewById(R.id.imgBtn_info).setOnClickListener(v ->
         {
 //            AudioHolder.PlaySfx(Sound.Standard);
 //            AudioHolder.PlayBgm();
             AudioHolder.mediaPlayerMusic.stop();
-            startActivity(new Intent(MainMenu.this, Credits.class));
+            startActivity(new Intent(v.getContext(), Credits.class));
         });
-        findViewById(R.id.imgBtn_settings).setOnClickListener(v ->
+        v.findViewById(R.id.imgBtn_settings).setOnClickListener(v ->
         {
-            if(AudioHolder.canPlaySFX) AudioHolder.PlaySfx(Sound.Standard);
-            if(AudioHolder.mediaPlayer.isPlaying()) AudioHolder.mediaPlayer.pause();
+            if (AudioHolder.canPlaySFX) AudioHolder.PlaySfx(Sound.Standard);
+            if (AudioHolder.mediaPlayer.isPlaying()) AudioHolder.mediaPlayer.pause();
             AudioHolder.mediaPlayerMusic.stop();
-            startActivity(new Intent(MainMenu.this, Preferencies.class));
+            startActivity(new Intent(v.getContext(), Preferencies.class));
         });
-        findViewById(R.id.imgBtn_exit).setOnClickListener(v ->
+        v.findViewById(R.id.imgBtn_exit).setOnClickListener(v ->
         {
             AudioHolder.mediaPlayerMusic.release();
             AudioHolder.soundPool.release();
             AudioHolder.mediaPlayer.release();
-            finishAffinity();
+            getActivity().finishAffinity();
         });
     }
 
@@ -99,11 +119,11 @@ public class MainMenu extends AppCompatActivity
      * fragments attached to the activity are <em>not</em> resumed.
      */
     @Override
-    protected void onResume()
+    public void onResume()
     {
         super.onResume();
 
-        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getContext());
 
         AudioHolder.canPlayBGM = pref.getBoolean("musica",true);
         AudioHolder.canPlaySFX = pref.getBoolean("sfx_btn",true);
